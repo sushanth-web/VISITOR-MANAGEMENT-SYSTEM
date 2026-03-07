719ec9d636252f9762e150dfe418e2f6736cf16b
Sushanth Vaddi <sushanthweb@gmail.com>
2026-03-07 11:18:56 +0530

    Update API base URL to production endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://visitor-management-system-b4lq.onrender.com/api',
 })
 api.interceptors.request.use(config=>{
     const token = localStorage.getItem('token')
